37e1ff87f683313f0c8caa50278362039129d559
CyanFlxy <xia_yuqiang@163.com>
2015-06-01 17:29:18 +0800

    研究编译环境

--- PeekerDemo/src/main/java/com/cyanflxy/peekerdemo/MainActivity.java
+++ PeekerDemo/src/main/java/com/cyanflxy/peekerdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.cyanflxy.peekerdemo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.cyanflxy.filepeeker.FilePeeker;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FilePeeker.listFile(this);
     }
 
 }
