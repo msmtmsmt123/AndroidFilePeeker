3caff835fc879f3508ec50c5678ce3b265498e8b
CyanFlxy <xia_yuqiang@163.com>
2015-06-23 18:09:12 +0800

    更新CopyRight

--- FilePeeker/src/main/java/com/cyanflxy/filepeeker/FilePeeker.java
+++ FilePeeker/src/main/java/com/cyanflxy/filepeeker/FilePeeker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 CyanFlxy <xyufeico@gmail.com>
+ * Copyright (C) 2015 CyanFlxy <cyanflxy@163.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
